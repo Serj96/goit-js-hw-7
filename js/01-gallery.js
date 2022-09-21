c2e4fd67bb21cdaeefc1570fd7f5b8b608334a66
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-09-21 22:42:10 +0300

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,22 +3,27 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const divRef = document.querySelector('.gallery');
+const imgMarkup = imgGallery(galleryItems);
 
-function imgGallery(items) {
-    return items.map((img) => `<div class="gallery__item">
-    <a class="gallery__link" href="${img.original}">
-      <img
+divRef.insertAdjacentHTML('beforeend', imgMarkup);
+
+function imgGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => 
+        
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
         class="gallery__image"
-        src="${img.preview}"
-        data-source="${img.original}"
-        alt="${img.description}"
-      />
-    </a>
-    </div>`).join("");
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`).join("");
+    
 }
 
-const addGalleryMarkup = imgGallery(galleryItems);
-divRef.innerHTML =  addGalleryMarkup;
+
 
 
 
