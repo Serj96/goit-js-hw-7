b18c776a5e955708d82f33a6bc1223aae4f1cf1a
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-09-21 22:21:29 +0300

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const divRef = document.querySelector('.gallery');
+
+function imgGallery(items) {
+    return items.map((img) => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+    </div>`).join("");
+}
+
+const addGalleryMarkup = imgGallery(galleryItems);
+divRef.innerHTML =  addGalleryMarkup;
+
+
+
+
