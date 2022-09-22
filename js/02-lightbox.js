832d2b1937ea2652c502f84cb19104849a1975f6
Serj96 <99637869+Serj96@users.noreply.github.com>
2022-09-22 19:28:50 +0300

    .

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function makeGalleryMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+        return `
+      <li class="gallery__item">
+      <a class="gallery__link" 
+      href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+      </a>
+      </li>`;
+    })
+    .join('');
+}
+
+const modal = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
 
 console.log(galleryItems);
